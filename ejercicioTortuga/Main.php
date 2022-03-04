e966df58e9abe90847800d0b65ca2f1995931c2f
saul <saulas73@educastur.es>
2022-03-04 15:28:16 +0100

    Commit inicial

---
+++ ejercicioTortuga/Main.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>
+        Unidad06Actividad01_02
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color">
+</head>
+
+<body>
+    <h1>Grupo 1.</h1>
+</body>
+
+</html>
