92b57bec6af882001d8263b2e6ce462d6a0b1daa
saul <saulas73@educastur.es>
2022-03-06 21:06:20 +0100

    [ARR] - Pinta ad,at,d,i sin error

--- ejercicioTortuga/Main.php
+++ ejercicioTortuga/Main.php
@@ -17,8 +17,11 @@
     <h1>Grupo 1.</h1>
     <?php
     $tamano=500;
-    $bbdd=[1,"at",50,2,"d",90,3,"at",50,4,"d",90,5,"at",50,6,"d",90,7,"at",50];
-    $bbdd[]=[0,"ti",50,0,"td",50,0,"ta",50];
+    // cuadrado arr iz// $bbdd=[1,"ad",50,2,"iz",90,3,"ad",50,4,"iz",90,5,"ad",50,6,"iz",90,7,"ad",50];
+    // cuadrado arr d// $bbdd=[1,"ad",50,2,"d",90,3,"ad",50,4,"d",90,5,"ad",50,6,"d",90,7,"ad",50];
+    // cuadrado aba d// $bbdd=[1,"at",50,2,"iz",90,3,"at",50,4,"iz",90,5,"at",50,6,"iz",90,7,"at",50];
+    // cuadrado aba iz// $bbdd=[1,"at",50,2,"d",90,3,"at",50,4,"d",90,5,"at",50,6,"d",90,7,"at",50];
+    $bbdd=[1,"ad",20,2,"iz",40,3,"ad",40,4,"d",10,5,"at",90,6,"d",20,7,"at",40];
     $array=[];
     for($i=0;$i<count($bbdd);$i+=3){
         $array[]=["id"=>$bbdd[$i],"comando"=>$bbdd[$i+1],"valor"=>$bbdd[$i+2]];
@@ -49,41 +52,41 @@
         }
         if ($array[$conteo]["comando"]=="ad") {
             if($grado==0){
-                $tan = $array[$conteo]["valor"];
+                $sin = $array[$conteo]["valor"];
                 $xf=$centrox;
-                $yf=$centroy-$tan;
+                $yf=$centroy-$sin;
             }elseif($grado==90){
                 $cos = $array[$conteo]["valor"];
                 $xf=$centrox+$cos;
                 $yf=$centroy;
             }elseif($grado==180){
-                $tan = $array[$conteo]["valor"];
+                $sin = $array[$conteo]["valor"];
                 $xf=$centrox;
-                $yf=$centroy+$tan;
+                $yf=$centroy+$sin;
             }elseif($grado==270){
                 $cos = $array[$conteo]["valor"];
                 $xf=$centrox-$cos;
                 $yf=$centroy;
             }elseif($grado<90&&$grado>0){
                 $cos = cos(90-$grado)*$array[$conteo]["valor"];
-                $tan = tan(90-$grado)*$array[$conteo]["valor"];
+                $sin = sin(90-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox+$cos;
-                $yf=$centroy-$tan;
+                $yf=$centroy-$sin;
             }elseif($grado<180&&$grado>90){
                 $cos = cos(180-$grado)*$array[$conteo]["valor"];
-                $tan = tan(180-$grado)*$array[$conteo]["valor"];
+                $sin = sin(180-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox+$cos;
-                $yf=$centroy+$tan;
+                $yf=$centroy+$sin;
             }elseif($grado<270&&$grado>180){
                 $cos = cos(270-$grado)*$array[$conteo]["valor"];
-                $tan = tan(270-$grado)*$array[$conteo]["valor"];
+                $sin = sin(270-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox-$cos;
-                $yf=$centroy+$tan;
+                $yf=$centroy+$sin;
             }elseif($grado<360&&$grado>270){
                 $cos = cos(360-$grado)*$array[$conteo]["valor"];
-                $tan = tan(360-$grado)*$array[$conteo]["valor"];
+                $sin = sin(360-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox-$cos;
-                $yf=$centroy-$tan;
+                $yf=$centroy-$sin;
             }
             $linea=("<line x1='$centrox' y1='$centroy' x2='$xf' y2='$yf' stroke='blue' stroke-width='5' />");
             $centrox=$xf;
@@ -92,41 +95,41 @@
         }
         if ($array[$conteo]["comando"]=="at") {
             if($grado==0){
-                $tan = $array[$conteo]["valor"];
+                $sin = $array[$conteo]["valor"];
                 $xf=$centrox;
-                $yf=$centroy+$tan;
+                $yf=$centroy+$sin;
             }elseif($grado==90){
                 $cos = $array[$conteo]["valor"];
                 $xf=$centrox-$cos;
                 $yf=$centroy;
             }elseif($grado==180){
-                $tan = $array[$conteo]["valor"];
+                $sin = $array[$conteo]["valor"];
                 $xf=$centrox;
-                $yf=$centroy-$tan;
+                $yf=$centroy-$sin;
             }elseif($grado==270){
                 $cos = $array[$conteo]["valor"];
                 $xf=$centrox+$cos;
                 $yf=$centroy;
             }elseif($grado<90&&$grado>0){
                 $cos = cos(90-$grado)*$array[$conteo]["valor"];
-                $tan = tan(90-$grado)*$array[$conteo]["valor"];
+                $sin = sin(90-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox+$cos;
-                $yf=$centroy-$tan;
+                $yf=$centroy-$sin;
             }elseif($grado<180&&$grado>90){
                 $cos = cos(180-$grado)*$array[$conteo]["valor"];
-                $tan = tan(180-$grado)*$array[$conteo]["valor"];
+                $sin = sin(180-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox+$cos;
-                $yf=$centroy+$tan;
+                $yf=$centroy+$sin;
             }elseif($grado<270&&$grado>180){
                 $cos = cos(270-$grado)*$array[$conteo]["valor"];
-                $tan = tan(270-$grado)*$array[$conteo]["valor"];
+                $sin = sin(270-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox-$cos;
-                $yf=$centroy+$tan;
+                $yf=$centroy+$sin;
             }elseif($grado<360&&$grado>270){
                 $cos = cos(360-$grado)*$array[$conteo]["valor"];
-                $tan = tan(360-$grado)*$array[$conteo]["valor"];
+                $sin = sin(360-$grado)*$array[$conteo]["valor"];
                 $xf=$centrox-$cos;
-                $yf=$centroy-$tan;
+                $yf=$centroy-$sin;
             }
             $linea=("<line x1='$centrox' y1='$centroy' x2='$xf' y2='$yf' stroke='black' stroke-width='5' />");
             $centrox=$xf;
