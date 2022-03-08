c796f7567334169c773b37b2a2830e44e8877901
Oscar Bueno <osbr2000@gmail.com>
2022-03-08 18:47:36 +0100

    commit

--- ejercicioTortuga/Main.php
+++ ejercicioTortuga/Main.php
@@ -13,6 +13,7 @@
 
 <body>
     <h1>Grupo 1.</h1>
+    <!--tiene 500 de ancho y de alto para poner la tortuga en la posicion 250,250 -->
     <svg width="500" height="500">
     <rect width="500" height="500" style="fill:rgb(255,240,255);stroke-width:5;stroke:rgb(0,0,0)" />
     </svg>
@@ -20,18 +21,23 @@
 
 if (isset($_POST['bCom'])){
     $strCom = $_POST['strCom'];
+    //patron para que si se pulsa el boton y no hay ningun comadno, pida introducir comandos
     if(empty($strCom)){
         echo "<p style='color:red'> *Introduzca algun comando* </p>";
-    } else if(preg_match('`[A-Z]`',$strCom)){
+        //patron para que el comando tenga que estar escrito en minusculas
+    } else if(preg_match('`[A-Z]`',$strCom)){ 
         echo "<p style='color:red'> *El comando debe estar escrito en minusculas *</p>";
-    } else if  (!preg_match('`[0-9]`',$strCom)) {
+        //patron para que el comando tenga que tener numeros
+    } else if  (!preg_match('`[0-9]`',$strCom)) { 
         echo "<p style='color:red'> *El comando debe tener valores numericos *</p>";
-    } else if(!preg_match('`[a-z]`',$strCom)){
+        //patron para que el comando tenga cualquier tipo de letra
+    } else if(!preg_match('`[a-z]`',$strCom)){ 
         echo "<p style='color:red'> *El comando debe tener letras *</p>";
     }
 }
 
     ?>
+    <!--formulario, metodo post -->
     <form action="" method="post">
     <input type="text" name="strCom" style="width=250" /><br />
     <br><input type="submit" name="bCom" value="Enviar" style="" />
