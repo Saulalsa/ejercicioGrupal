161b0f9320359ea189edcadf2698ac81393ce968
alvaroIBQ <98331374+alvaroIBQ@users.noreply.github.com>
2022-03-13 02:55:02 +0100

    [MOD]repite funciona de **** madre :)
    Se pueden meter comandos complementarios antes t después de repite
    ad 100 repite 4 (ad 50 de 90)
    repite 4 (ad 50 de 90) ad 100
    repite 4 (ad 50 de 90)

--- ejercicioTortuga/variables.php
+++ ejercicioTortuga/variables.php
@@ -8,6 +8,47 @@ $arrayBBDD = []; //Array Final para la BBDD [comando,valor]
 if (isset($_POST["strCom"])) {
 
     $arrayStr = explode(" ", $_POST["strCom"]);
+
+    //    ##### ----- ##### REPEAT ##### ----- #####
+
+    $repeat = explode("repite", $_POST["strCom"]);
+    //print_r($arrayStr);
+    if (count($repeat) > 1) {
+
+        //print_r($repeat);
+        $str1 = $repeat[1];
+        $limpia1 = explode("(", $str1);
+        $str2 = $limpia1[1];
+        $limpia2 = explode(")", $str2);
+
+        //print_r($limpia1[0]); //el nº de veces que repite
+        //print_r($limpia2[1]); //comandos a repetir
+
+        //limpio el array para mantener el orden
+        $arrayStr=[];
+
+        //Se pueden poner comandos antes y despues del repite
+        $repeatInit=explode(" ",$repeat[0]);
+        //print_r($repeatInit);//al hacer el explode, el ultimo 
+        //indice esta vacio y se controla en el for restando 2
+        for ($i=0; $i<=(count($repeatInit)-2);$i++) {
+            $arrayStr[]=$repeatInit[$i];
+        }
+        for ($n = 0;$n<$limpia1[0];$n++) {
+            foreach (explode(" ",$limpia2[0]) as $comando) {
+                $arrayStr[]=$comando;
+            }
+        }
+        $repeatFinal=explode(" ",$limpia2[1]);
+        //print_r($repeatFinal);//al hacer el explode, el primer 
+        //indice esta vacio y se controla en el inicio > 0
+        for ($i=1; $i<=(count($repeatFinal)-1);$i++) {
+            $arrayStr[]=$repeatFinal[$i];
+        }
+    }
+    //print_r($arrayStr);
+
+
     $contador = 0;
     $contadorDefault = 0;
     $ad = [];
@@ -22,6 +63,8 @@ if (isset($_POST["strCom"])) {
     $casa = [];
     $valoresInt = [];
     $default = [];
+
+
     foreach ($arrayStr as $dato) {
         //Cuando convertimos en int una string no numerica el valor es 0
         $saleInt = (int) $dato; //integer
@@ -90,7 +133,7 @@ if (isset($_POST["strCom"])) {
     $dsn = "mysql:dbname=logoBBDD;host=db"; //dsn con nombre de la bd de datos creada
     $usuarioBD = "alumnado";
     $claveBD = "alumnado";
-    for ($i = 0; $i+1 < count($arrayBBDD); $i = $i + 2) {
+    for ($i = 0; $i + 1 < count($arrayBBDD); $i = $i + 2) {
         try {
             $bd = new PDO($dsn, $usuarioBD, $claveBD);
             $sql2 = "INSERT INTO comandos (comando, valor, id_user) VALUES
@@ -100,7 +143,6 @@ if (isset($_POST["strCom"])) {
         } catch (PDOException $e) {
         }
     }
+    //print_r($arrayBBDD);
     $arrayBBDD = [];
-}
-else {
-}
+}else {}
