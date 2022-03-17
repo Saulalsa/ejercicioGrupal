ccb7661171881506a26003fc7b41108c9395cd2b
Oscar Bueno <osbr2000@gmail.com>
2022-03-17 13:32:16 +0100

    commit

--- ejercicioTortuga/crudUsuarios.php
+++ ejercicioTortuga/crudUsuarios.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="crudUsuarios.css">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
 <?php
     //Saul Alonso Sanchez Grupo 1
     $dsn = "mysql:dbname=logoBBDD;host=db";
@@ -5,7 +17,7 @@
     $claveBD = "alumnado";
     try {
         $bd = new PDO($dsn, $usuarioBD, $claveBD);
-        $sql3 = "SELECT * FROM comandos";
+        $sql3 = "SELECT * FROM usuarios";
         $resultadoObtener = $bd->query($sql3);
 
         $array = [];
@@ -15,14 +27,13 @@
             $fila['perfil'] = $elemento['perfil'];
             $fila['nombre'] = $elemento['nombre'];
             $fila['pass'] = $elemento['pass'];
-            $fila['desc'] = $elemento['desc'];
+            $fila['descripcion'] = $elemento['descripcion'];
             $fila['activo'] = $elemento['activo'];
             $array[] = $fila;
         }
     } catch (PDOException $e) {
     }
-
-    // $array=[0=>['id_user'=>'1','perfil'=>'prof','nombre'=>'saul','pass'=>'saul','desc'=>'saul alonso sanchez','activo'=>'false'],1=>['id_user'=>'2','perfil'=>'alum','nombre'=>'pedro','pass'=>'pedro','desc'=>'pedro rajoi casado','activo'=>'true']];
+    // $array=[0=>['id_user'=>'1','perfil'=>'prof','nombre'=>'saul','pass'=>'saul','descripcion'=>'saul alonso sanchez','activo'=>'false'],1=>['id_user'=>'2','perfil'=>'alum','nombre'=>'pedro','pass'=>'pedro','descripcion'=>'pedro rajoi casado','activo'=>'true']];
     echo("<table border='black solid 1px'>");
     //Linea de la tabla con la leyenda
     echo("<tr>");
@@ -39,12 +50,18 @@
         echo('pass');
         echo("</td>");
         echo("<td>");
-        echo('desccripcion');
+        echo('descripcioncripcion');
         echo("</td>");
         echo("<td>");
         echo('activo');
         echo("</td>");
-        echo("</tr>");
+        echo("<td>");
+        echo('editar');
+        echo("</td>");
+        echo("<td>");
+        echo('borrar');
+        echo("</td>");
+    echo("</tr>");
         //bucle para crear todas las linea de las tablas
     foreach ($array as $valor) {
         echo("<tr>");
@@ -61,31 +78,32 @@
         echo($valor['pass']);
         echo("</td>");
         echo("<td>");
-        echo($valor['desc']);
+        echo($valor['descripcion']);
         echo("</td>");
         echo("<td>");
         //if para colocar el check box activo o no
-        if ($valor['activo']=='true') {
-            echo("<input type='checkbox' id='".$valor['id_user']."' checked disabled/>");
-        }elseif ($valor['activo']=='false') {
-            echo("<input type='checkbox' id='".$valor['id_user']."' disabled/>");
+        if ($valor['activo']==true) {
+            echo("<input type='checkbox' checked disabled/>");
+        }elseif ($valor['activo']==false) {
+            echo("<input type='checkbox' disabled/>");
         }
         echo("</td>");
         //creacion de formulario para acceder al editar usuario
-        echo("<form action='edit.php' method='post'>");
+        echo("<form action='editar.php' method='post'>");
         echo("<td>");
-        echo("<input type='submit' name='editar".$valor['id_user']."' value='editar'/>");
+        echo("<input type='submit' name='".$valor['id_user']."' value='editar'/>");
         echo("</td>");
         echo("</form>");
         //creacion de formulario para acceder a borrar usuario
         echo("<form action='borrar.php' method='post'>");
         echo("<td>");
-        echo("<input type='submit' name='borrar".$valor['id_user']."' value='borrar'/>");
+        echo("<input type='submit' name='".$valor['id_user']."' value='borrar'/>");
         echo("</td>");
         echo("</form>");
         echo("</tr>");
     }
     echo("</table>");
+    echo("<br>");
     //creacion de formulario para acceder a crear usuario
     echo("<form action='crear.php' method='post'>");
     echo("<input type='submit' name='crear' value='Crear Usuario' id='crearUser'>");
